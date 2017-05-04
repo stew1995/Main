09976d502705b6dc41fd62faea8a1021877b2ce1
stew1995 <up703451@myport.ac.uk>
2017-05-04 15:53:59 +0100

    Updated

--- js/chat.js
+++ js/chat.js
@@ -9,16 +9,19 @@ $(document).ready(
   var root = firebase.database().ref().child("Messages");
 
   root.on("child_added", snap => {
-    $("#chatLayout").append("<div id='messageContent'><span class='timestamp'>"+snap.val().Time+"</span><p class='messageAuthor'>"+snap.val().User+"</p><p class='content'>"+snap.val().Message+"</p>");
+    $("#chatLayout").append("<div class='messagecontainor'><div class='cardview teal z-depth-3'>"+
+        "<div class='sender'>"+
+          "<span class='white-text'>"+snap.val().User+"</span><span class='right white-text'>"+snap.val().Time+"</span>"+
+        "</div><div class='messagecontent'><span>"+snap.val().Message+"</span></div></div></div>"
+      );
   })
 });
-
 
 $(document).ready(function() {
   $(".sendButton").click( function() {
     var message = $("#messageinput");
     if(message.val() !="") {
-      writeMessage(message);
+      writeMessage(message.val());
     }
     message.val("");
   })
@@ -39,7 +42,7 @@ function writeMessage(message) {
       Message: message,
       Time: formatTime,
       //Wont work as no user is signed in
-  //    User : user.uid
+      //User : user.uid
       User : "Stewart"
     });
 }
@@ -48,3 +51,8 @@ function writeMessage(message) {
 //Need to do layout of the message
 // validator
 // spacing ect
+$(".testbutton").click(function() {
+  $('.tap-target').tapTarget('open');
+});
+$('.tap-target').tapTarget('open');
+  $('.tap-target').tapTarget('close');
