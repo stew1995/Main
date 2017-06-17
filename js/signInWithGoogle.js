fe05b94b4f5cc5ac1843e0ebd2df04c9d834e3a7
stew1995 <up703451@myport.ac.uk>
2017-06-17 09:59:33 +0100

    Made login work with google, ish

--- js/signInWithGoogle.js
+++ js/signInWithGoogle.js
@@ -1,22 +1,31 @@
-var profile;
+var googleUser;
+var auth2;
+
+//Authentication Listeners
+
+
+
 function onSignIn(googleUser) {
 
-  $("#body").load("checkProfile.html", function () {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log(profile.getGivenName());
+  console.log(profile.getFamilyName());
+
+  $("#body").load("main2.html");
+  /*$("#body").load("checkProfile.html", function () {
     /*var elm = document.createElement('script');
     elm.src = "js/signInWithGoogle.js";
     body.appendChild(elm);
 
   /*  map = document.createElement('script');
     map.src = "js/map.js";
-    body.appendChild(map);*/
+    body.appendChild(map);
 
-    profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    console.log(profile.getGivenName());
-    console.log(profile.getFamilyName());
+
 
     $('.userData').append('<div><img id="googleImage" src="'+profile.getImageUrl()+'"></div>'+
     '<div class="row">'+
@@ -46,23 +55,12 @@ function onSignIn(googleUser) {
 
   /*  '<div id="userDataContainor"><h5 class="center">Name</h5><p class="center flow-text" id="googleName">'+profile.getName()+'</p>'+
     '<h5 class="center">Email</h5><p class="center flow-text" id="googleEmail">'+profile.getEmail()+'</p></div>');
-*/
-})
+
+})*/
+
 
 
 }
-
-
-function formSubmit() {
-  alert("Submitted");
-  //GitCheck
-}
-  //User data in database after send, now all i need to do is show it, but to also implement the chat feature
-
-
-
-
-
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
@@ -73,3 +71,10 @@ function signOut() {
       console.log('User signed out.');
     });
   }
+
+
+function formSubmit() {
+  alert("Submitted");
+  //GitCheck
+}
+  //User data in database after send, now all i need to do is show it, but to also implement the chat feature
